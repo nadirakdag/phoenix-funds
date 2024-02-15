b028cafe454e406ea9e868861b6306c2b3e1be16
nadir.akdag <nadir.akdag@getmidas.com>
2024-02-15 15:55:36 +0300

    added

---
+++ src/main/java/dev/nadirakdag/phoenixfunds/PhoenixFundsApplication.java
@@ -0,0 +1,13 @@
+package dev.nadirakdag.phoenixfunds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixFundsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoenixFundsApplication.class, args);
+    }
+
+}
